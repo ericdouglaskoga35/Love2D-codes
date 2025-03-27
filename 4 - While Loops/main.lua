156c1c5895d3633fdf6daafedf673ade8219ea96
Eric Douglas Koga <claireredfieldbr18@gmail.com>
2025-03-27 12:19:48 -0300

    Commit de atualizado do laço while

---
+++ 4 - While Loops/main.lua
@@ -0,0 +1,12 @@
+message = 0
+test = 0
+
+while message < 100 do
+    message = message + 1
+    test = test - 5
+end
+
+function love.draw()
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(test)
+end
