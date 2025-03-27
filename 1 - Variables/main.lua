1a8631f5ae25f2652b90e6ed130fe54dd4cf5c38
Eric Douglas Koga <claireredfieldbr18@gmail.com>
2025-03-27 07:07:18 -0300

    My first commit in Lua codes

---
+++ 1 - Variables/main.lua
@@ -0,0 +1,10 @@
+message = 5
+chicken = 10
+output = chicken + 1 
+
+message = message - 1
+
+function love.draw()
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message)
+end
---
+++ 2 - If Statements/main.lua
@@ -0,0 +1,11 @@
+message = 0
+condition = 25
+
+if condition > 0 then
+    message = 1
+end
+
+function love.draw()
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message)
+end
